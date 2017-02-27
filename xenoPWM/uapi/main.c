a9c60d832131c50174c3647aa78398a8fdb72d41
oiz5201618 <oiz5201618@gmail.com>
2017-02-27 18:45:09 +0800

    Add the user space program to access the xenomai module.

---
+++ xenoPWM/uapi/main.c
@@ -0,0 +1,39 @@
+/*
+	Access RTDM device for software PWM from user space.
+	Shao-Hua, Wang
+	2017.02.24
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <string.h>
+
+#include "pwm_user.h"
+#include "xenopwm.h"
+
+#define PIN1 16
+#define PIN2 21
+
+int filp;
+
+int main() {
+
+	filp = open("/dev/rtdm/xenoPWM", 0);
+	if(filp < 0) {
+		printf("failed to open device xenoPWM : %s\n", strerror(errno));
+	}
+
+	pinMode(filp, PIN1, OUTPUT);
+	pinMode(filp, PIN2, OUTPUT);
+
+	softPwmCreate(filp, PIN1, 0, 100);
+	softPwmCreate(filp, PIN2, 0, 10000);
+
+	softPwmWrite(filp, PIN1, 50);
+	softPwmWrite(filp, PIN2, 1000);
+
+	return 0;
+}
---
+++ xenoPWM/uapi/pwm_user.c
@@ -0,0 +1,75 @@
+/*
+	Access RTDM device for software PWM from user space.
+	Shao-Hua, Wang
+	2017.02.24
+*/
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <string.h>
+
+#include "pwm_user.h"
+#include "xenopwm.h"
+
+void pinMode(int filp, int pin, int type) {
+
+	int ret;
+	int tmp[2];
+
+	tmp[0] = pin;
+	tmp[1] = type;
+
+	ret = ioctl(filp, PIN_SET, tmp);
+	if (ret < 0) {
+		printf("ioctl for PIN_SET failed: %d %s\n", ret, strerror(errno));
+	}
+
+}
+
+void digitalWrite(int filp, int pin, int status) {
+
+	int ret;
+	int tmp[2];
+
+	tmp[0] = pin;
+	tmp[1] = status;
+
+	ret = ioctl(filp, GPIO_SET, tmp);
+	if (ret < 0) {
+		printf("ioctl for GPIO_SET failed: %d %s\n", ret, strerror(errno));
+	}
+
+}
+
+void softPwmCreate(int filp, int pin, int min, int max) {
+
+	int ret;
+	int tmp[3];
+
+	tmp[0] = pin;
+	tmp[1] = min;
+	tmp[2] = max;
+
+	ret = ioctl(filp, PWM_INIT, tmp);
+	if (ret < 0) {
+		printf("ioctl for PWM_INIT failed: %d %s\n", ret, strerror(errno));
+	}
+
+}
+
+void softPwmWrite(int filp, int pin, int duty_cycle) {
+
+	int ret;
+	int tmp[2];
+
+	tmp[0] = pin;
+	tmp[1] = duty_cycle;
+
+	ret = ioctl(filp, PWM_SET, tmp);
+	if (ret < 0) {
+		printf("ioctl for PWM_SET failed: %d %s\n", ret, strerror(errno));
+	}
+
+}
